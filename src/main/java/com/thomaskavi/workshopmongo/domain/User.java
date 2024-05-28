0851e985ad22fbd4444db10f6c6240dd8e020634
thomaskavi <thomaskavi2000@gmail.com>
2024-05-28 17:08:48 -0300

    Referenciando os posts do usuário

--- src/main/java/com/thomaskavi/workshopmongo/domain/User.java
+++ src/main/java/com/thomaskavi/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.thomaskavi.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 // A anotação @Document indica que esta classe será mapeada para uma coleção no MongoDB.
@@ -17,6 +20,8 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 
 	// Construtor padrão (sem argumentos), necessário para algumas operações de
 	// serialização e deserialização.
@@ -55,6 +60,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	// Sobrescrita do método hashCode para gerar um código hash baseado no campo id.
